6a6ed60e4087a80cf239e2d2b3d1dd26b6ca0df4
jnwooo <95736158+jnwooo@users.noreply.github.com>
2023-10-31 13:38:52 +0900

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 import streamlit as st
 from PIL import Image
+from io import BytesIO
+import base64
 
 st.set_page_config(layout="wide", page_title="# MS AI SCHOOL")
 
-st.write("## View Your Image")
-st.write(": 이미지를 올리세요")
+st.write("## MS AI SCHOOL : AI 생활 폐기물 분류 시스템")
+st.write(": Upload your trash Image :fire")
 
 st.sidebar.write("## Upload and Download :gear:")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
+# Download the detected image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
 def fix_image(upload):
     image = Image.open(upload)
     col1.write("Original Image :camera:")
     col1.image(image)
+
+    col2.write("detected Image :wrench:")
+    col2.image(image)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download detected image", convert_image(image), "bbox.png", "image/png")
+
 
 col1, col2 = st.columns(2)
 
