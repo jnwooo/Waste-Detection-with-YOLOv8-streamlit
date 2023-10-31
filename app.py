37dcb8ce665668e8559501ad7546f80bfdb5df30
jnwooo <95736158+jnwooo@users.noreply.github.com>
2023-10-31 11:34:15 +0900

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,28 @@
 import streamlit as st
-st.write('# MS AI SCHOOL')
-st.write(' ## 1조 - 객체인식 프로젝트 ')
-st.write('### streamlit_test')
+from PIL import Image
+
+st.set_page_config(layout="wide", page_title="# MS AI SCHOOL")
+
+st.write("## View Your Image")
+st.write(": 이미지를 올리세요")
+
+st.sidebar.write("## Upload and Download :gear:")
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+col1, col2 = st.columns(2)
+
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    st.sidebar.markdown("\n")
+    st.sidebar.write("No image uploaded.")
