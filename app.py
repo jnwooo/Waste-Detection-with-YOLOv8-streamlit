a6abbede2bb4245fecb830c22bae5dc48490f8d7
jnwooo <95736158+jnwooo@users.noreply.github.com>
2023-11-02 16:42:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,113 @@
+# Python In-built packages
+from pathlib import Path
+import PIL
+
+# External packages
 import streamlit as st
-import os
-from PIL import Image
-from io import BytesIO
-import base64
-import cv2  # OpenCV 라이브러리를 가져옵니다.
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-st.set_page_config(layout="wide", page_title="# MS AI SCHOOL")
+# Local Modules
+import settings
+import helper
 
-st.write("# MS AI SCHOOL 1조 : AI 생활 폐기물 분류 시스템")
-st.write("## : Upload your trash Image :fire:")
 
-st.sidebar.write("## Upload and Download :gear:")
+# Setting page layout
+st.set_page_config(
+    page_title="AI 생활 폐기물 분류 시스템",
+    page_icon="🤖",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+# Main page heading
+st.title("AI 생활 폐기물 분류 시스템")
 
-# Download the detected image
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
+# Sidebar header
+st.sidebar.header("Upload trash images or Videos :fire:")
 
-def fix_image(upload):
-    image = Image.open(upload)
-    col1.write("Original Image :camera:")
-    col1.image(image)
+# Model Options
+model_type = st.sidebar.radio(
+    "Object detection", ['Detection'])
 
-    # 이미지를 좌우 반전합니다.
-    image_cv2 = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)  # PIL 이미지를 OpenCV 형식으로 변환합니다.
-    flipped_image = cv2.flip(image_cv2, 1)  # 이미지를 좌우로 반전시킵니다.
-    flipped_image_pil = Image.fromarray(cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB))  # 다시 PIL 형식으로 변환합니다.
-    col2.write("Detected Image :wrench:")  # 좌우 반전한 이미지를 표시합니다.
-    col2.image(flipped_image_pil)
 
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download detected image", convert_image(flipped_image_pil), "bbox.png", "image/png")
+# Sidebar slider
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 25, 100, 40)) / 100
 
-col1, col2 = st.columns(2)
 
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-if my_upload is not None:
-    if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
-    else:
-        fix_image(upload=my_upload)
+# Selecting Detection Or Segmentation
+if model_type == 'Detection':
+    model_path = Path(settings.DETECTION_MODEL)
+
+
+# Load Pre-trained ML Model
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    st.error(f"Unable to load model. Check the specified path: {model_path}")
+    st.error(ex)
+
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+source_img = None
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        try:
+            if source_img is None:
+                default_image_path = str(settings.DEFAULT_IMAGE)
+                default_image = PIL.Image.open(default_image_path)
+                st.image(default_image_path, caption="Default Image",
+                         use_column_width=True)
+            else:
+                uploaded_image = PIL.Image.open(source_img)
+                st.image(source_img, caption="Uploaded Image",
+                         use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+    with col2:
+        if source_img is None:
+            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+            default_detected_image = PIL.Image.open(
+                default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
+        else:
+            if st.sidebar.button('Detect Objects'):
+                res = model.predict(uploaded_image, conf=confidence)
+                boxes = res[0].boxes
+                if len(boxes) == 0:
+                    st.error("No objects detected in the image.")
+                else:
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                            use_column_width=True)
+                    try:
+                        with st.expander("Detection Results"):
+                            for box in boxes:
+                                st.write(box.data)
+                    except Exception as ex:
+                        st.write("No image is uploaded yet!")
+
+
+elif source_radio == settings.VIDEO:
+    helper.play_stored_video(confidence, model)
+
+elif source_radio == settings.WEBCAM:
+    helper.play_webcam(confidence, model)
+
+elif source_radio == settings.YOUTUBE:
+    helper.play_youtube_video(confidence, model)
+
 else:
-    st.sidebar.markdown("\n")
-    st.sidebar.write("No image uploaded.")
+    st.error("Please select a valid source type!")
