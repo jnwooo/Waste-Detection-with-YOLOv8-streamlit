b3912d2cd7c4a7a5b95ee1b2d558b3f1cc1e426a
jnwooo <95736158+jnwooo@users.noreply.github.com>
2023-11-03 16:12:01 +0900

    Update helper.py

--- helper.py
+++ helper.py
@@ -22,12 +22,7 @@ def load_model(model_path):
 
 
 def display_tracker_options():
-    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
-    is_display_tracker = True if display_tracker == 'Yes' else False
-    if is_display_tracker:
-        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
-        return is_display_tracker, tracker_type
-    return is_display_tracker, None
+    pass
 
 
 def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
