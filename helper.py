29aa72931f02572b6a0f93c1f8b0ddc0155f69e1
jnwooo <95736158+jnwooo@users.noreply.github.com>
2023-11-03 17:57:42 +0900

    Update helper.py

--- helper.py
+++ helper.py
@@ -8,16 +8,7 @@ import settings
 
 
 def load_model(model_path):
-    """
-    Loads a YOLO object detection model from the specified model_path.
-
-    Parameters:
-        model_path (str): The path to the YOLO model file.
-
-    Returns:
-        A YOLO object detection model.
-    """
-    model = YOLO(model_path)
+     model = YOLO(model_path)
     return model
 
 
